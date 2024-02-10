4b35ef22d0b57fb115cf9337f0d82b76fe346521
Jaemarlee Goin <150928570+Jaemarlee@users.noreply.github.com>
2024-02-10 01:48:02 +0000

    logic finished, just need to add styling

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
+// Create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("");
+	const [todos, setTodos] = useState([]);
+	const handleDelete = index => {
+		setTodos(todos.filter((_, i) => i !== index));
+	  };
+	  
+	const handleKeyDown = e => {
+		if (e.keyCode === 13 && inputValue.trim() !== "") {
+			// Check if the pressed key is Enter and inputValue is not empty
+			setTodos([...todos, inputValue.trim()]); // Add trimmed inputValue to todos
+			setInputValue(""); // Clear inputValue after adding todo
+		}
+	};
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1>My To Do's</h1>
+			<ul>
+				<li>
+					<input
+						type="text"
+						onChange={e => setInputValue(e.target.value)}
+						value={inputValue}
+						onKeyDown={handleKeyDown} // Use handleKeyDown function
+						placeholder="What needs to be done?"
+					/>
+				</li>
+				{todos.map((todo, index) => (
+					<li key={index}>
+						{todo}
+						<button onClick={() => handleDelete(index)}>
+							<FontAwesomeIcon icon={faTrashCan} />
+						</button>
+					</li>
+				))}
+			</ul>
+			<div>{todos.length} tasks</div>
 		</div>
 	);
 };
